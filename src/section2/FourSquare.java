0af07d1449da4f68bfddfc719a94b08c06724c58
ilabstaff <ilabstaff@172.16.116.73>
2019-07-08 17:56:02 -0700

    fourSquare.

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,26 +6,30 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+	Robot never = new Robot();
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+never.setSpeed(100);
 		// 5. Set the pen width to 5
-
+ 
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
 
 			// 7. Set the pen color to random
 	
 			// 1. Call the drawSquare() method
-	
+drawSquare();
+	{	
+
+	}
 			// 8. Turn the robot 90 degrees to the right
 
 	}
 
 	
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
+		never.move(20);
+		never.turn(90);
 		
 		
 	}
--- src/section2/PentagonCrazy.java
+++ src/section2/PentagonCrazy.java
@@ -10,32 +10,40 @@ public class PentagonCrazy {
 
 	private void makePrettyThings() {
 		// 1. Create a new Robot
-
+		Robot never = new Robot();
 		// 3. Put the robot's pen down
-
+		never.penDown();
 		// 8. Make the robot go at maximum speed (100)
-
+never.setSpeed(100);
 		// 9. Set the pen to a color that you like for the shape
-
+never.setRandomPenColor();
 		// 4. Make a variable for the number of sides you want (can’t test this one)
-
-		// 5. Make a second variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6
-
-		// 7. Use a for loop to repeat steps #2 to #11, 200 times. When this is done you should see a pentagon
+		int sides = 19;
+		// 5. Make a second variable for the angle you want the robot to turn. Hint: you
+		// can divide in Java using "/". Can’t test until step 6
+		int angle = 999 / sides;
+		// 7. Use a for loop to repeat steps #2 to #11, 200 times. When this is done you
+		// should see a pentagon
+		for (int i = 0; i < 1998; i++) {
 
 			// 2. Move the robot 200 pixels
-	
-			// 10. Change the previous line of code to make the robot move "i" pixels instead of 200 
-	
-			// 6. Turn the robot the amount in your angle variable
-	
-			// 11. Turn the robot one more degree
+			never.move(i);
 
+			// 10. Change the previous line of code to make the robot move "i" pixels
+			// instead of 200
+
+			// 6. Turn the robot the amount in your angle variable
+			never.turn(angle);
+			// 11. Turn the robot one more degree
+never.turn(1);
+		}
 	}
 
 	// Variations:
 	// *12. make the pattern really huge
-	// *13. randomize the color of the pattern 
+	
+	// *13. randomize the color of the pattern
+	
 	// *14. experiment with different shapes
 
 	public static void main(String[] args) {
