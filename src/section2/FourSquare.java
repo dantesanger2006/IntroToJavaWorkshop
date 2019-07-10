2867d5cc1fbcf44035a812ff30a25a2dee5d5546
ilabstaff <ilabstaff@172.16.116.67>
2019-07-09 17:58:23 -0700

    triangleshell

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -9,31 +9,47 @@ public class FourSquare {
 	Robot never = new Robot();
 	void go() {
 		// 4. Make the robot move as fast as possible
-never.setSpeed(100);
+		never.setSpeed(100);
 		// 5. Set the pen width to 5
- 
+			never.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+			for (int i = 0; i < 4.; i++) {
+				
+			
 			// 7. Set the pen color to random
-	
-			// 1. Call the drawSquare() method
-drawSquare();
+			never.setRandomPenColor();
+				// 8. Turn the robot 90 degrees to the right
+				never.turn(90);
+			
+			}	
+			
+				
+				// 1. Call the drawSquare() method		
+		drawSquare();
 	{	
 
 	}
-			// 8. Turn the robot 90 degrees to the right
-
+			
+			
 	}
 
 	
 	void drawSquare() {
 		/* 3. Fill in the code to draw a square inside the method below. */
-		never.move(20);
+		never.penDown();
+		never.move(100);
 		never.turn(90);
+		never.move(100);
+		never.turn(90);
+		never.move(100);
+		never.turn(90);
+		never.move(100);
 		
 		
+	
 	}
-
+	
+	
 	public static void main(String[] args) {
 		new FourSquare().go();
 	}
--- src/section2/TriangleShell.java
+++ src/section2/TriangleShell.java
@@ -5,32 +5,47 @@ import org.jointheleague.graphical.robot.Robot;
 public class TriangleShell {
 
 	// 1. Create a new Robot
-
+	Robot never = new Robot();
 	
 	void go() {
-		drawTriangle(100); // 3. delete this line (used only for testing)
+		 // 3. delete this line (used only for testing)
 
 		// 6. Make the robot go as fast as possible
-
+		never.setSpeed(100);
 		// 4. make a variable to hold the length of the triangle and set it to 50
-
+		int no = 50;
 		// 7. Use a for loop to repeat steps #9 to #10, 60 times
-
-			// 9. Change the color of the pen to a random color
-	
-			// 8. Increase the length variable by 10
-	
-			// 5. call your drawTriangle() method using your length variable
-	
+		for (int i = 0; i < 60; i++) {
+			
+		
+		
+		// 8. Increase the length variable by 10
+			no =no+10;
+		// 5. call your drawTriangle() method using your length variable
+					drawTriangle(no);
+		
+		
+		// 9. Change the color of the pen to a random color
+			never.setRandomPenColor();
+			
+			
+			
 			// 10. Turn the robot 6 degrees to the right
-
+			never.turn(6);
+			
+		}
 	}
 
 	/* 2. fill in the method below to draw a triangle. Use the length variable when you call move(). */
 	private void drawTriangle(int length) {
-		
-		
-		
+	never.penDown();
+	never.turn(360/3);
+	never.move(length);
+	never.turn(360/3);
+	never.move(length);
+	never.turn(360/3);
+	never.move(length);
+	
 	}
 
 	
